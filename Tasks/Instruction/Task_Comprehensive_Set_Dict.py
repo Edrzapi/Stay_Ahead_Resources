c8e8ff51e0ec30c555d26988a4dd5b6360bbed9c
Edrzapi <admin@devfoundry.co.uk>
2026-09-10 14:19:24 +0100

    adding comprenhension set/dict task

---
+++ Tasks/Instruction/Task_Comprehensive_Set_Dict.py
@@ -0,0 +1,28 @@
+names = [
+    "alice",
+    "bob",
+    "alice",
+    "charlie",
+    "bob",
+    "dave"
+]
+
+
+# TODO:
+# Create a set called unique_names
+# containing each name only once
+
+unique_names = ...
+
+
+# TODO:
+# Create a dictionary called name_lookup
+# where:
+# key = the name
+# value = the name converted to uppercase
+
+name_lookup = ...
+
+
+print(unique_names)
+print(name_lookup)
