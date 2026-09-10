b243642a5ba55747691aeb0ffc32f1a558147502
Edrzapi <admin@devfoundry.co.uk>
2026-09-10 13:33:36 +0100

    adding comprenhension task

---
+++ Tasks/Instruction/Task_Comprehensive_books.py
@@ -0,0 +1,13 @@
+
+books = {id: 1, 'title': 'The Gruffalo', 'author': 'Dondalson', 'genre': 'Childrens', 'ratings': [4,5,5]}
+
+
+# Titles only
+titles = [b['title'] for b in books]
+# children books only
+childrens = [b for b in books if]
+# Title of children books only
+# educational and military book
+# title and author of classics
+# books with avg rating > 4
+# title and author of children books with avg > 4
